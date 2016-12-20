9a77e12e9cfdee1a394b2e268d194b8f76b6af67
Brandon Hinds <brandonhinds@Jasons-MacBook-Air.local>
2016-12-20 15:49:12 -0500

    Initial Commit

---
+++ content.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+	var link = "";
+
+	chrome.tabs.getSelected(null, function(tab){
+        link = tab.url;
+
+        $.ajax(link, {
+        	method: 'GET'
+        }).done(function(res){
+        	var list = "<ul>";
+        	var notes = $(res).find(".modtype_resource");
+
+        	$.each(notes, function(k, v){
+        		list += "<li class='item'>";
+        		list += "<input class='check' type='checkbox'/>";
+        		var a = $(this).find("a").attr("href");
+        		list += "<a class='note' href='"+a+"' download='"+a+"'>" + $(this).text() +"</a>";
+        		list += "</li><br>";
+        	});
+
+        	list += "</ul>";
+
+        	$("#main").html(list);
+        	
+        }).fail(function(err){
+        	$("#main").html("<b>Error Occured</b>");
+        });
+    });
+
+    //select all items
+
+    $("#all").on('click', function(){
+    	$(".check").attr("checked", true);
+
+    });
+
+    //download selected items
+
+    $("#don").on('click', function(){
+    	$.each($(".item"), function(){
+    		var check = this.getElementsByClassName('check')[0];
+    		if (check.checked == true){
+    			var dl = this.getElementsByClassName('note')[0];
+    			dl.click();
+    		}
+    	});
+    });
+});
